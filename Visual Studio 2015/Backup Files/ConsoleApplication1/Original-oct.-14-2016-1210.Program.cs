4e007c904802a0bd105b14edc509813eb538910a
Tom Apter <Tom Apter>
2016-10-14 12:49:12 +0200

    merge

---
+++ Visual Studio 2015/Backup Files/ConsoleApplication1/Original-oct.-14-2016-1210.Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static int Fact(int number)
+        {
+            if (number == 0)
+                return 1;
+            else
+                return number * Fact(number - 1);
+
+        }
+        static void ex1(int [] tab)
+        {
+            for (int i = 0; i < tab.Length; i++)
+            {
+                Console.Write(tab[i] + "|");
+            }
+            Console.WriteLine();
+            Random shuffle = new Random();
+            for (int i = 0; i < tab.Length; i++)
+            {
+                int nextindex = shuffle.Next(tab.Length);
+                int tmp = tab[i];
+                tab[i] = tab[nextindex];
+                tab[nextindex] = tmp;
+            }
+            for (int i = 0; i < tab.Length; i++)
+            {
+                Console.Write(tab[i] + "|");
+            }
+
+        }
+        static int ex2(int number)
+        {
+            Console.WriteLine(Fact(number));
+            int count = 0;
+            int a = Fact(number);
+            while (a % 5 == 0)
+            {
+                a = a / 5;
+                count++;
+            }
+            return count; 
+        }
+        //static int exDate(int day, int month, int year)
+
+        static int Alien(int X, int Y)
+        {
+            int[] tab = new int [32];
+            int eggs = 0;
+            int alien = 1;
+            tab[0] = 1;
+            for (int i = 1; i<tab.Length - 1; i++)
+            {
+                eggs = tab[i] * X;
+                if (i % Y == 0)
+                {
+                    alien = eggs * (Y - 1) + tab[i - Y];
+                }
+
+
+                tab[i + 1] = alien;
+                Console.Write( alien + "|");
+            }
+
+
+            return alien;
+        }
+        
+        static void Main(string[] args)
+        {
+            int [] tab = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+            //ex1(tab);
+            Console.WriteLine("\n"+Alien(3,5));
+            
+        }
+    }
+}
