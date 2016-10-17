c97ad8010c2cebedf37aafbacd40137dd0c9ff79
Tom Apter <Tom Apter>
2016-10-17 15:24:39 +0200

    merge

---
+++ Visual Studio 2015/Projects/ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Week_2
+{
+    class Program
+    {
+        static int PerfectSquare(int n)
+        {
+            int square = 1;
+            for(int i = 0; i <= n; i++)
+            {
+                if (Math.Sqrt(i) % 1 == 0)
+                {
+                    square = i;
+                }
+            }
+            return square;
+        }
+        static void Main(string[] args)
+        {
+            Console.Write(PerfectSquare(78));
+        }
+    }
+}
